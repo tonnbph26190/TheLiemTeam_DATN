5d6cfac45a0e1540f7af572a2659abd5cd3920d1
Sotatek-ton.nguyen <ton.nguyen@sotatek.com>
2025-05-04 17:49:36 +0700

    chac khong sao dau

--- PresentationLayer/wwwroot/asset_client/js/main.js
+++ PresentationLayer/wwwroot/asset_client/js/main.js
@@ -161,23 +161,21 @@
         Quantity change
     --------------------- */
     var proQty = $('.pro-qty');
-    proQty.prepend('<span class="fa fa-angle-up dec qtybtn"></span>');
-    proQty.append('<span class="fa fa-angle-down inc qtybtn"></span>');
+
     proQty.on('click', '.qtybtn', function () {
         var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
+        var $input = $button.closest('.pro-qty').find('input');
+        var oldValue = parseFloat($input.val());
+
         if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
+            var newVal = oldValue + 1;
         } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
+            var newVal = oldValue > 0 ? oldValue - 1 : 0;
         }
-        $button.parent().find('input').val(newVal);
+
+        $input.val(newVal);
     });
+
 
     var proQty = $('.pro-qty-2');
     proQty.prepend('<span class="fa fa-angle-left dec qtybtn"></span>');
